c113f602ce4694d6c42a1b5c913cfd258a7be443
Mateo Cante Garzón <123739687+Mateo1205@users.noreply.github.com>
2024-04-17 01:22:20 -0500

    Add files via upload

---
+++ Serie.js
@@ -0,0 +1,13 @@
+var Serie = /** @class */ (function () {
+    function Serie(id, title, network, seasons, description, website, imageUrl) {
+        this.id = id;
+        this.title = title;
+        this.network = network;
+        this.seasons = seasons;
+        this.description = description;
+        this.website = website;
+        this.imageUrl = imageUrl;
+    }
+    return Serie;
+}());
+export { Serie };
---
+++ Serie.ts
@@ -0,0 +1,19 @@
+export class Serie {
+    id: number;
+    title: string;
+    network: string;
+    seasons: number;
+    description: string;
+    website: string;
+    imageUrl: string;
+
+    constructor(id: number, title: string, network: string, seasons: number, description: string, website: string, imageUrl: string) {
+        this.id = id;
+        this.title = title;
+        this.network = network;
+        this.seasons = seasons;
+        this.description = description;
+        this.website = website;
+        this.imageUrl = imageUrl;
+    }
+}
